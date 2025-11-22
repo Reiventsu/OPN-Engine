40dc86972c3d776824c2e88146e4234722d2a60a
reiven <137615290+Reiventsu@users.noreply.github.com>
2025-11-22 23:39:11 +0100

    fixed spacing and replaces includes

--- Source/opnEngine/Core/System/JobDispatcher.h
+++ Source/opnEngine/Core/System/JobDispatcher.h
@@ -2,21 +2,23 @@
 
 #include <functional>
 #include <atomic>
-#include <vector>
 #include <thread>
-#include <future>
+#include <utility>
 
 #include "Thread/SPSCQueue.h"
 
-namespace opn {
-    enum class eJobType {
+namespace opn
+{
+    enum class eJobType
+    {
         General,
         Asset,
         Audio,
         Render,
     };
 
-    struct sTask {
+    struct sTask
+    {
         std::function<void()> execute;
         uint32_t fenceID = 0;
 
@@ -26,23 +28,80 @@ namespace opn {
     /**
      * @brief Monostate class.
      */
-    class JobDispatcher {
+    class JobDispatcher
+    {
         static constexpr size_t MAX_FENCES = 4096;
         static constexpr size_t QUEUE_SIZE = 1024;
 
     public:
-        static void initialize() {
-            for (auto &f: s_fencePool) {
-                f.store(0, std::memory_order_relaxed);
+        static void initialize()
+        {
+            for (auto& f : s_fencePool)
+            {
+                f.store(0, std::memory_order_release);
             }
         }
 
-        template<typename Func, typename... Args>
-        static uint32_t Submit(eJobType _type, Func &&_func, Args &&... args) {
-            uint32_t fenceID = s_nextFenceID.fetch_add(1) % MAX_FENCES;
+        template <typename Func, typename... Args>
+        static uint32_t Submit(const eJobType _type, Func&& _func, Args&&... _args)
+        {
+            const uint32_t fenceID = s_nextFenceID.fetch_add(1) % MAX_FENCES;
             s_fencePool[fenceID].store(1, std::memory_order_release);
 
             sTask newTask;
+
+            newTask.fenceID = fenceID;
+            newTask.execute = [function = std::forward<Func>(_func), ..._args = std::forward<Args>(_args)]() mutable
+            {
+                std::invoke(function, std::move(_args)...);
+            };
+
+            bool success = false;
+            switch (_type)
+            {
+            case eJobType::General: success = s_generalQueue << std::move(newTask);
+                break;
+            case eJobType::Asset: success = s_assetQueue << std::move(newTask);
+                break;
+            case eJobType::Audio: success = s_audioQueue << std::move(newTask);
+                break;
+            case eJobType::Render: success = s_renderQueue << std::move(newTask);
+                break;
+            }
+            if (!success)
+            {
+                s_fencePool[fenceID].store(0, std::memory_order_release);
+                return 0;
+            }
+            return fenceID;
+        }
+
+        static bool isFenceSignaled(const uint32_t _fenceID) noexcept
+        {
+            return s_fencePool[_fenceID % MAX_FENCES].load(std::memory_order_acquire) == 0;
+        }
+
+        static void waitForFence(const uint32_t _fenceID) noexcept
+        {
+            s_fencePool[_fenceID % MAX_FENCES].wait(1, std::memory_order_acquire);
+        }
+
+        static SPSCQueue<sTask, QUEUE_SIZE>& getQueue(const eJobType _type) noexcept
+        {
+            switch (_type)
+            {
+            case eJobType::General: return s_generalQueue;
+            case eJobType::Asset: return s_assetQueue;
+            case eJobType::Audio: return s_audioQueue;
+            case eJobType::Render: return s_renderQueue;
+            default: return s_generalQueue;
+            }
+        }
+
+        static void signalCompletion(const uint32_t _fenceID) noexcept
+        {
+            s_fencePool[_fenceID % MAX_FENCES].store(0, std::memory_order_release);
+            s_fencePool[_fenceID % MAX_FENCES].notify_one();
         }
 
     private:
