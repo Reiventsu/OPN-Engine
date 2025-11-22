25362d57df611cfa015f24e41871c3ab105f0900
reiven <137615290+Reiventsu@users.noreply.github.com>
2025-11-22 23:39:29 +0100

    iAsset should be looking good now

---
+++ Source/opnEngine/Core/System/Asset/iAsset.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <atomic>
+#include <string>
+
+#include "System/UUID.h"
+
+namespace opn::AssetData
+{
+    enum class eAssetType
+    {
+        Texture,
+        Model,
+        Material,
+        Shader,
+        Animation,
+        Audio,
+        Font,
+        Scene,
+        Prefab,
+    };
+
+    class iAsset
+    {
+    public:
+        virtual ~iAsset() = default;
+
+        UUID        uuid;
+        eAssetType  type;
+        std::string name;
+        std::string path;
+
+        std::atomic<bool> isCPULoaded = false;
+        std::atomic<bool> isGPULoaded = false;
+
+    protected:
+        iAsset(const eAssetType _type, const std::string_view _name, const std::string_view _path)
+            : type(_type), name(_name), path(_path)
+        {
+        }
+    };
+}
